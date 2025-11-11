9247a4785f1956016a29f7a67cbe032fa3788fb6
Luisa García <luisa.garcia1331@gmail.com>
2025-11-10 21:15:03 -0500

    codificación de módulos del software según requerimientos del proyecto

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String description;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String description, double price) {
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
